7bf4e3c17d350fa63b6e0c8881e331026dfc6668
garlee2 <leegaryhaig@gmail.com>
2020-11-04 13:29:12 -0800

    code refactor

--- test_parser.py
+++ test_parser.py
@@ -4,6 +4,7 @@ with open("config.text", "r") as fd:
 			deviceConfig = fd.read()
 
 
+
 class ConfigurationParser:	
 	def parseCustomerNames(self):
 		customerNamePattern = r'ip vrf ([a-zA-Z_]+)\n'
@@ -20,24 +21,23 @@ class ConfigurationParser:
 		customerIPs = re.findall(customerIPPattern, deviceConfig)
 		return customerIPs
 
+
 class TestParse(unittest.TestCase):
+	cp = ConfigurationParser()
 	def test_parse_cust_name(self):
-		cp = ConfigurationParser()
 		expected_names = ['CUSTOMER_A', 'CUSTOMER_B']
-		parsed_names = cp.parseCustomerNames()
+		parsed_names = self.cp.parseCustomerNames()
 		self.assertEqual(list, type(parsed_names))
 		self.assertEqual(expected_names, parsed_names)
 
 	def test_parse_cust_vlan(self):
-		cp = ConfigurationParser()
 		expected_vlans = ['100', '101']
-		parsed_vlans = cp.parseCustomerVLAN()
+		parsed_vlans = self.cp.parseCustomerVLAN()
 		self.assertEqual(list, type(parsed_vlans))
 		self.assertEqual(expected_vlans, parsed_vlans)
 
 	def test_parse_cust_ip(self):
-		cp = ConfigurationParser()
 		expected_ips = ['10.10.100.1', '10.10.101.1']
-		parsed_ips = cp.parseCustomerIP()
+		parsed_ips = self.cp.parseCustomerIP()
 		self.assertEqual(list, type(parsed_ips))
 		self.assertEqual(expected_ips, parsed_ips)
